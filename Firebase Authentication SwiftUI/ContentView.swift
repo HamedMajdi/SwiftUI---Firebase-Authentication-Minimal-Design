b615f76710595c42237fa9c7c905185071bbda78
hamedmajdi <hamed90_viva@yahoo.com>
2024-03-07 21:32:25 +0300

    Initial Commit

---
+++ Firebase Authentication SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Firebase Authentication SwiftUI
+//
+//  Created by Hamed Majdi on 3/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Firebase Authentication SwiftUI/Firebase_Authentication_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Authentication_SwiftUIApp.swift
+//  Firebase Authentication SwiftUI
+//
+//  Created by Hamed Majdi on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Authentication_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
